ada5f8406a37646b4ab99c2e149c9e057642ce16
Pier Paolo Pazzi <99829016+pierpaolopazzi@users.noreply.github.com>
2023-07-07 19:15:47 +0200

    Update gcc.pl

--- gcc/gcc.pl
+++ gcc/gcc.pl
@@ -1,3 +1,11 @@
+% This simple problem show how to use the predicate gcc. 
+% We have 7 people and they must be assigned to 5 working shifts.
+% So, domains stand for:
+% 1: Morning; 2: Day; 3: Night; 4: Backup; 5: day-off
+% eg. gcc(1,2,1) stands for: 
+% the Morning shifts (third argument) can be occupied by a lower bound (1) to an upper bound (2) of people
+
+
 :- lib(fd).
 :- lib(fd_global_gac).
 
