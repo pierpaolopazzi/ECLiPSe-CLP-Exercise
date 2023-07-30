5e080aa89f91371ba640426b9a011a3d19990c6d
Pier Paolo Pazzi <99829016+pierpaolopazzi@users.noreply.github.com>
2023-07-30 17:57:09 +0200

    Add files via upload

---
+++ Routing/infermiera.pl
@@ -0,0 +1,54 @@
+:- lib(ic).
+:- lib(branch_and_bound).
+:- lib(propia).
+:- [pazienti].
+
+
+infermiera(PathL):-
+	
+
+	findall(ID, paziente(ID,_,_), PazList),
+	length(PazList, NumeroPaz),
+	Nplace is NumeroPaz+1,
+	length(PathL,Nplace),
+	domain(PathL, NumeroPaz),
+	alldifferent(PathL),
+	applyDistaConstr(PathL,Dist,0),	
+	minimize(labeling(PathL),Dist).
+	
+	
+domain([],_).
+domain([HPathL|TPathL], NumeroPaz):-
+	HPathL=0,
+	TPathL::1..NumeroPaz.
+
+applyDistaConstr([Last], Dist, CostIter):-
+	distanza(Last,0,Cost) infers ac,
+	Dist #= CostIter + Cost.
+applyDistaConstr([ThisPat, NextPat|Lpat], Dist, CostIter):-
+	distanza(ThisPat, NextPat, Cost) infers ac,
+	paziente(NextPat, MinTime, MaxTime) infers ac,
+	CostSucc #= Cost + CostIter,
+	CostSucc #>= MinTime,
+	CostSucc #=< MaxTime,
+	applyDistaConstr([NextPat|Lpat], Dist, CostSucc).
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
